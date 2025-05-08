fd1636f98eaf7d129b63f27e813ece39f66b438e
PierreLgol <plgol.perso@gmail.com>
2025-05-08 15:54:05 +0200

    update

--- main.zig
+++ main.zig
@@ -1,14 +1,16 @@
 const std = @import("std");
 
+fn uart_print(msg: []const u8) void {
+    const UART_FIFO: *volatile u8 = @ptrFromInt(0x3FF40000);
+    for (msg) |c| {
+        UART_FIFO.* = c;
+    }
+}
+
 pub export fn call_start_cpu0() void {
-    const UART_FIFO: *volatile u32 = @ptrFromInt(0x3FF40000);
-    UART_FIFO.* = 'H';
-    UART_FIFO.* = 'i';
-    UART_FIFO.* = '\n';
+    uart_print("Booting microkernel...\n");
 
     while (true) {
-        UART_FIFO.* = 'H';
-        UART_FIFO.* = 'i';
-        UART_FIFO.* = '\n';
+        uart_print("hi\n");
     }
 }
